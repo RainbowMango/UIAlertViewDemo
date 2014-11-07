c1a6743ba0e4d0f16942b797f3b867d292e487a0
RainbowMango <qdurenhongcai@163.com>
2014-11-07 21:39:23 +0800

    Initial tab bar based application

---
+++ UIAlertViewDemo/UIAlertViewDemo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  UIAlertViewDemo
+//
+//  Created by ruby on 14-11-7.
+//  Copyright (c) 2014年 ruby. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
