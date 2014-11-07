20c5156da69e77b4522168572115ad99f9fba088
RainbowMango <qdurenhongcai@163.com>
2014-11-07 23:30:34 +0800

    Finished Demo

--- UIAlertViewDemo/UIAlertViewDemo/FirstViewController.swift
+++ UIAlertViewDemo/UIAlertViewDemo/FirstViewController.swift
@@ -20,6 +20,32 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func popButtonTouchUpInside(sender: AnyObject) {
+        // 弹出最默认的Alert
+        var myDefaultAlert: UIAlertView! = UIAlertView(title: "AlertDemo", message: "使用storyboard实现", delegate: nil, cancelButtonTitle: "Cancel")
+        myDefaultAlert.alertViewStyle = UIAlertViewStyle.Default
+        myDefaultAlert.show()
+        
+        // 弹出带有密码输入框的Alert
+        var mySecureAlert: UIAlertView! = UIAlertView(title: "AlertDemo", message: "使用storyboard实现", delegate: nil, cancelButtonTitle: "Cancel")
+        mySecureAlert.alertViewStyle = UIAlertViewStyle.SecureTextInput
+        mySecureAlert.show()
+        
+        // 弹出带有普通输入框的Alert
+        var myPlainAlert: UIAlertView! = UIAlertView(title: "AlertDemo", message: "使用storyboard实现", delegate: nil, cancelButtonTitle: "Cancel")
+        myPlainAlert.alertViewStyle = UIAlertViewStyle.PlainTextInput
+        myPlainAlert.show()
+        
+        // 弹出带有账号和密码输入框的Alert
+        var myLoginAlert: UIAlertView! = UIAlertView(title: "AlertDemo", message: "使用storyboard实现", delegate: nil, cancelButtonTitle: "Cancel")
+        myLoginAlert.alertViewStyle = UIAlertViewStyle.LoginAndPasswordInput
+        myLoginAlert.show()
+        
+        // 弹出带有两个按钮的Alert
+        var myTwoButtonAlert: UIAlertView! = UIAlertView(title: "AlertDemo", message: "使用storyboard实现", delegate: nil, cancelButtonTitle: "Cancel")
+        myTwoButtonAlert.addButtonWithTitle("Sure")
+        myTwoButtonAlert.show()
+    }
 
 }
 
--- UIAlertViewDemo/UIAlertViewDemo/SecondViewController.swift
+++ UIAlertViewDemo/UIAlertViewDemo/SecondViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    var myButton: UIButton!
+    var myLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // 创建按钮并设置属性
+        myButton = UIButton(frame: CGRectMake(100, 100, 195, 30))
+        myButton.setTitle("点击弹出Alert", forState: UIControlState.Normal)
+        myButton.setTitleColor(UIColor.blueColor(), forState: UIControlState.Normal)
+        
+        // 创建标签并设置属性
+        myLabel = UILabel(frame: CGRectMake(100, 150, 195, 17))
+        myLabel.textColor = UIColor.blackColor()
+        myLabel.text = "使用storyboard实现"
+        
+        // 将按钮和标签添加到View
+        self.view.addSubview(myButton)
+        self.view.addSubview(myLabel)
+        
+        // 添加按钮的点击动作
+        myButton.addTarget(self, action: "popButtonTouchUpInside:", forControlEvents: UIControlEvents.TouchUpInside)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +39,12 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func popButtonTouchUpInside(sender: AnyObject) {
+        // 弹出最默认的Alert
+        var myDefaultAlert: UIAlertView! = UIAlertView(title: "AlertDemo", message: "使用storyboard实现", delegate: nil, cancelButtonTitle: "Cancel")
+        myDefaultAlert.alertViewStyle = UIAlertViewStyle.Default
+        myDefaultAlert.show()
+    }
 
 }
 
